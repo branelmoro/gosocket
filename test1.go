531beb99fdd9cc7d486583363e5274668fb31c4b
branelmoro <branelmoro@gmail.com>
2018-07-04 09:25:54 +0530

    minor change

--- test1.go
+++ test1.go
@@ -1,20 +1,48 @@
 package main
 
+import "compress/flate"
+// import "log"
+import "bytes"
+// import "os"
+// import "io/ioutil"
+import "io"
 import "fmt"
 
 
-type abc struct{
-	a int
-	b int
+// 0x48 0x65 0x6c 0x6c 0x6f (contains "Hello")
+// 0xf2 0x48 0xcd 0xc9 0xc9 0x07 0x00 (compressed)
+
+func main() {
+    // inData, _ := ioutil.ReadFile("stuff.dat")
+
+// [72 101 108 108 111] ---- hello
+// [242 72 205 201 201 7 4 0 0 255 255] ---- compressed
+// 0xf2 0x48 0xcd 0xc9 0xc9 0x07
+
+    inData := []byte{72,101,108,108,111}
+
+    compressedData := new(bytes.Buffer) 
+
+    // compressedData := make([]byte, 100)
+    compress(inData, compressedData, 9)
+    // fmt.Println(byte(compressedData))
+    fmt.Println(compressedData.Bytes())
+
+    // ioutil.WriteFile("compressed.dat", compressedData.Bytes(), os.ModeAppend)
+
+    deCompressedData := new(bytes.Buffer)
+    decompress(compressedData, deCompressedData)
+    // log.Print(deCompressedData)
+    fmt.Println(deCompressedData.Bytes())
+    // fmt.Println(byte(deCompressedData))
 }
-
-
-func main(){
-	a := abc{a:10,b:20}
-	// a.a := 10
-
-	b := &a
-
-	b.a = 11
-	fmt.Println(a)
+func compress(src []byte, dest io.Writer, level int) {
+    compressor, _ := flate.NewWriter(dest, level)
+    compressor.Write(src)
+    compressor.Close()
+}
+func decompress(src io.Reader, dest io.Writer) {
+    decompressor := flate.NewReader(src)
+    io.Copy(dest, decompressor)
+    decompressor.Close()
 }
